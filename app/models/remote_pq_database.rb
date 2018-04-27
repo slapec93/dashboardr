125096e74fadd79cedf70fb305b4e8ade7cce6b6
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-04-27 14:01:17 +0200

    Add comment to Postgres connection class

--- app/models/remote_pq_database.rb
+++ app/models/remote_pq_database.rb
@@ -1,4 +1,5 @@
 class RemotePQDatabase < ActiveRecord::Base
     self.abstract_class = true
+    # Establish the connection to the Postgres database
     establish_connection REMOTE_PQ_DATABASE
 end
