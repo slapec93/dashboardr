89632012436c3a347b18e6650b4a22d3b2b78e1e
Gergely Békési <slapec93@users.noreply.github.com>
2019-03-27 09:44:33 +0100

    Update remote_pq_database.rb

--- app/models/remote_pq_database.rb
+++ app/models/remote_pq_database.rb
@@ -1,5 +1,5 @@
 class RemotePQDatabase < ActiveRecord::Base
     self.abstract_class = true
-    # Establish the connection to the almighty Postgres database
+    # Establish the connection to the almighty Postgres database 
     establish_connection REMOTE_PQ_DATABASE
 end
